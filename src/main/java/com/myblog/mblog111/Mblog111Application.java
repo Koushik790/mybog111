ce14fc4cf9e5105ef5f572487f0dd5661a70f799
Koushik <koushikdey1244@gmail.com>
2024-07-21 11:47:32 +0530

    127:Create save Post Features

---
+++ src/main/java/com/myblog/mblog111/Mblog111Application.java
@@ -0,0 +1,13 @@
+package com.myblog.mblog111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mblog111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mblog111Application.class, args);
+	}
+
+}
